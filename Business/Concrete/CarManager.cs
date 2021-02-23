b4641cf43ede8bca2b1e1230307870d2678e9406
ilaydaez64 <ilaydaez64@gmail.com>
2021-02-24 01:16:00 +0300

    ValidationAspect eklendi

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -30,10 +31,10 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Car>>(_cars.GetAll(c => c.CarID == carID));
         }
 
-
+        [ValidationAspect(typeof(CarValidator))]
         public IResult Add(Car car)
         {
-            ValidationTool.Validate(new CarValidator(), car);
+
             _cars.Add(car);
 
             return new SuccessResult(Massages.CarAdded);
